82426841aaea14cf27820b084d09d507adc88cbf
shubhamk840 <shubhamk840@gmail.com>
2021-05-17 11:14:17 +0530

    added more platforms

--- app.js
+++ app.js
@@ -16,19 +16,27 @@ app.use(favicon(__dirname+'/public/favicon/favicon.ico'))
 
 app.use(express.urlencoded({extended:false}));
 
-// const Register = require('./src/db/models/register');
 
-//For static website 
 app.use(express.static(static_path));
 
 app.get('/',(req,res)=>{
       res.render('index');
 })
-// Creating a new user in database
 app.post('/register',async (req,res)=>{
+      if(req.body.platform=="codechef"){
     const values = req.body.fname;
     res.redirect("https://codechef.com/users/"+values);
+      }
+      else if(req.body.platform=="codeforces"){
+            const values = req.body.fname;
+            res.redirect("https://codeforces.com/profile/"+values)
+      }
+      else if(req.body.platform=="atcoder"){
+            const values = req.body.fname;
+            res.redirect("https://atcoder.jp/users/"+values);
+      }
 })
+
 
 app.listen(port,()=>{
       console.log("The port has been succesfully connected");
